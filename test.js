400583b4b732df027eea680290d57cb74e05c62c
ath4lia <acmarfil@up.edu.ph>
2025-03-06 23:08:07 +0800

    test: added test cases

--- test.js
+++ test.js
@@ -1,8 +1,14 @@
 import needle from "needle";
 
-// needle.get("http://localhost:3000/find-by-author", (err, res) => {
-//     console.log(res.body); // prints the body of the response message. In
-//     });
+needle.get("http://localhost:3000/find-by-author?author=J.K+Rowling", (err, res) => {
+    console.log(res.body); 
+    });
+
+
+needle.get("http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&author=J.K+Rowling", (err, res) => {
+    console.log(res.body); 
+    });
+
 
 needle.post("http://localhost:3000/add-book", 
 {
@@ -14,3 +20,15 @@ needle.post("http://localhost:3000/add-book",
 (err, res) => {
 console.log(res.body)
 });
+
+
+needle.post("http://localhost:3000/add-book", 
+    {
+        bookName: "Jane Eyre",
+        ISBN: "978-0-1414-4114-6",
+        author: "Charlotte Bronte",
+        yearPublished: "1847",
+    },
+    (err, res) => {
+    console.log(res.body)
+    });
